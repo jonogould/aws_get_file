3b3f8bacf7433ccef2536481110b597e1a979cf6
Jono Gould <jono@gould.co.za>
2013-05-03 12:38:01 +0200

    Get in first

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 aws_get_file
 ============
 
-Function to download files from AWS S3.
+by Jono Gould ([TravelGround](http://github.com/TravelGround)), @jonogould
+
+Use this function to download files from your s3 bucket. Requires [knox](https://github.com/LearnBoost/knox).
+
+## Installation
+
+First, you need to install [node](http://nodejs.org). Then you can continue!
+
+This script runs in terminal.
+
+To get these tools running, you need the following dependencies, available off npm:
+
+
+- [commander](http://visionmedia.github.com/commander.js)
+- [shelljs](http://shelljs.org)
+- [underscorejs](http://underscorejs.org)
+- [cli-color](https://npmjs.org/package/cli-color)
+- [awalk](https://github.com/TravelGround/awalk)
+- [utils](https://github.com/TravelGround/utils)
+
+To install this dependency (the easy way), run the following command from the ``` aws_get_file ``` root dir:
+
+``` $ npm install ```
+
+Or place the dependencies there manually but adding them to a ``` node_modules ``` folder
+
+Or add them individually by calling:
+
+``` $ npm install underscore ```
+
+Lastly make the script executable using ```chmod +x aws_get_file.js```
+
+
+## Usage
+
+How do you use ``` aws_get_file ```, I hear you asking? Well:
+
+- Open terminal
+- Navigate to the ``` aws_get_file ``` root dir
+- Type ``` ./aws_get_file ``` and press enter
+- There are your helpful instructions :D
+
+## Examples
+
+Check out the [wiki](https://github.com/TravelGround/aws_get_file/wiki) for some examples on what this tool could be used for, check it out and add your own!
+
+
+### Now go play, use it to create useful stuff!
---
+++ aws_get_file.js
@@ -0,0 +1,85 @@
+/*
+*
+*	Description:
+*	Function to download files from AWS S3
+*	
+*	Use this function to download files from your s3 bucket. Requires knox.
+*	Make an argument null if not in use.
+*
+*	Usage: 				aws_get_file(array_of_files, null, root, destination_dir, aws_connection, function(results){});
+*	or from txt file: 	aws_get_file(null, 'test.txt', root, destination_dir, aws_connection, function(results){
+*
+*	path 				= shell.cat('list.txt').split('\n');
+*	file_name 			= './downloads'
+*	root 				= The host where files will start being saved
+*	destination_dir 	= The path where files are saved. * NOTE: make 'null' to copy input file's path structure
+*	aws_connection 		= The knox client connection, specified with knox.createClient({key, secret, bucket});
+*	done 				= function that is called when job is done
+*/
+
+
+//	Dependancies
+var fs = require('fs');
+var url = require('url');
+var shell = require('shelljs');
+var _ = require('underscore');
+
+
+var aws_get_file = function (path, file_name, root, destination_dir, aws_connection, done) {
+	//	If the file_name is set, overwrite the path array with this
+	if (file_name) {
+		path = shell.cat(file_name).split('\n');
+	}
+
+	var batch_size = 100;
+	var outstanding = 0;
+	var open_files = 0;
+
+	var written = [];
+	var missed = [];
+
+	_.each(path, function(f) {
+		if (f.length === 0)
+			return done([]);
+
+		var filename = f.split('/').pop();
+		var destination_file = destination_dir + filename;
+		var new_file = '';
+
+		//	Check if the destination dir is set, if not then copy the current f path
+		if (destination_dir === null) {
+			destination_file = url.parse(f).pathname;
+		}
+
+		if (open_files < batch_size) {
+			open_files++;
+			//	Now grab the file from s3
+			aws_connection.getFile(f, function(err, res) {
+				if(err) return err;
+
+				res.setEncoding('binary');
+				
+				res.on('data', function(chunk) {
+					new_file += chunk;
+				});
+
+				res.on('end', function(chunk) {
+					//	WRITE FILE
+					fs.writeFile(root + '/' + destination_file, new_file, 'binary', function (err) {
+						written.push(f + '\n');
+						outstanding++;
+						open_files--;
+						
+						if (outstanding === batch_size) done({written: written, missed: missed});
+					});
+				});
+			});
+		}
+		else {
+			missed.push(f + '\n');
+		}
+	});
+};
+
+
+exports.aws_get_file = aws_get_file;
